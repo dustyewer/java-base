a61e94c01808b8190a7638a1cc4b90288c789e38
dustyewer <dustyewer@aliyun.com>
2021-04-05 15:55:40 +0800

    try

--- try-catch-finally/src/main/java/com/yewer/FinallyTest.java
+++ try-catch-finally/src/main/java/com/yewer/FinallyTest.java
@@ -5,39 +5,30 @@ import java.util.Scanner;
 public class FinallyTest {
   public static void main(String[] args) {
     //
-      Scanner scanner=new Scanner(System.in);
-      String line;
-      while(scanner.hasNextLine()){
-          line=scanner.nextLine();
+    Scanner scanner = new Scanner(System.in);
+    String line;
+    while (scanner.hasNextLine()) {
+      line = scanner.nextLine();
 
-          System.out.println(tryFun(line));
-
-
-      }
-
-
-
+      System.out.println(tryFun(line));
+    }
   }
 
-  public static int tryFun(String line){
-      try{
-          System.out.println("try:"+Integer.parseInt(line));
-          return 1;
-      }
-//      catch(Exception e){
-//          System.out.println("catch"+e);
-//          return 2;
-//      }
-      finally{
-          System.out.println("finally");
-          //不要在finall里面return
-         return 3;
-          //不要在finally里面抛异常
-//          throw new RuntimeException();
-      }
+  public static int tryFun(String line) {
+    try {
+      System.out.println("try:" + Integer.parseInt(line));
+      return 1;
+    }
+    //      catch(Exception e){
+    //          System.out.println("catch"+e);
+    //          return 2;
+    //      }
+    finally {
+      System.out.println("finally");
+      // 不要在finall里面return
+      return 3;
+      // 不要在finally里面抛异常
+      //          throw new RuntimeException();
+    }
   }
-
-
-
-
 }
